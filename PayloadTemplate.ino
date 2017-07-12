5970bdd5bce19a6e6a42f458da71977c386b4cfb
Ryan Bowers <rbowers314@gmail.com>
2017-07-12 10:48:42 -0600

    Initial Commit

---
+++ PayloadTemplate.ino
@@ -0,0 +1,17 @@
+/* Code to run a basic payload box featuring SD datalogging, GPS tracking, xBee communication, status LEDs, and additional sensors
+ * Author: Ryan Bowers
+ */
+
+#include <SD.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+#include <Relay_XBee.h>
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
+
